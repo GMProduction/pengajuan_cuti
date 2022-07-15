89669e10add62730d4b08bdaa30ac8366d50dc59
portgazdbeel <portgaz77@gmail.com>
2022-07-15 13:28:43 +0700

    commit

--- database/migrations/2022_07_15_055844_add_soft_delete_users.php
+++ database/migrations/2022_07_15_055844_add_soft_delete_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DbProject extends Migration
+class AddSoftDeleteUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class DbProject extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->softDeletes();
+        });
     }
 
     /**
@@ -23,6 +26,9 @@ class DbProject extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropSoftDeletes();
+        });
     }
 }
